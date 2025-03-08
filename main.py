ee538b2d5981273c587c3cb025b4588f2db97935
Jorge <Jorgeburgos300@gmail.com>
2025-03-08 16:44:16 -0500

    Se programo la parte de pedir los datos para la factura y cuando esta excede los 500.000

---
+++ main.py
@@ -0,0 +1,14 @@
+M = int(input("Digite la cantidad de piezas a comprar: "))
+i = int(input("Digite el precio unitario: "))
+c = M*i
+
+if c > 500000:
+    h = c * 0.55
+    e = c * 0.3
+    ll = c - (h+e)
+    C = ll * 0.2
+    a = ll + c
+    print(f'El monto total de la compra es de: {c}')
+    print(f'La inversión de la empresa debe ser de: {h}')
+    print(f'El prestamo del banco debe ser de: {e}')
+    print(f'El credito a solicitar al fabricante (con el 20% aplicado) debe ser de: {a}')
